c20cc3efbccf8a50485a9f3cdddd03eea57ce31d
kinosist <kinosist@hotmail.co.jp>
2024-10-21 10:35:01 +0900

    リスタートボタンの設置

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,15 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     private GameObject text;
+    private GameObject restartButton;
 
     // Start is called before the first frame update
     void Start()
     {
         text = GameObject.Find("Text");
+        restartButton = GameObject.Find("RestartButton");
+
+        // ボタンを非表示
+        restartButton.SetActive(false);
     }
 
     // Update is called once per frame
@@ -23,5 +29,15 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
         text.GetComponent<Text>().text = "Game Over";
+
+        // ボタンを表示
+        restartButton.SetActive(true);
+    }
+
+    // リスタート処理
+    public void Restart()
+    {
+        // 現在のシーンを再読み込み
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
